1f576b6d242cdb468d124154df665809d5120653
Marvin <yma010@ucr.edu>
2020-04-03 14:35:37 -0700

    added test routes for users and weapons

--- app.js
+++ app.js
@@ -2,18 +2,18 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 const db = process.env.MONGO_URI || require('./config/keys').mongoURI;
+const users = require("./routes/api/users");
+const weapons = require('./routes/api/weapons');
 
 mongoose
     .connect(db, { useNewUrlParser: true })
     .then(() => console.log('Connected to MongoDB Successfully'))
     .catch(err => console.log(err));
 
-
-
-
 app.get('/', (req, res) => res.send("Hello World"));
 
-
+app.use('/api/users', users);
+app.use('/api/weapons', weapons);
 
 
 const port = process.env.PORT || 5000;
