55fa826bc19d8f201c8cfef56829a6efcc928979
Marvin <yma010@ucr.edu>
2020-04-03 14:20:28 -0700

    added production keys

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
+const db = process.env.MONGO_URI || require('./config/keys').mongoURI;
+
+mongoose
+    .connect(db, { useNewUrlParser: true })
+    .then(() => console.log('Connected to MongoDB Successfully'))
+    .catch(err => console.log(err));
+
+
+
+
+app.get('/', (req, res) => res.send("Hello World"));
+
+
+
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is listening on port ${port}`));
-
-app.get('/', (req, res) => res.send("Hello World"));
