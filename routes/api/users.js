a657a128ad0ed1b2bcb0570dc76481227bb1c79a
Marvin <yma010@ucr.edu>
2020-04-03 15:20:26 -0700

    added user login/registration routes and user model

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,55 @@
 const express = require('express');
 const router = express.Router();
+const validateRegisterInput = require('../../validations/register');
+const validateLoginInput = require('../../validations/login');
+
+
+router.post('/register', (req, res) => {
+    const {err, isValid} = validateRegisterInput(req.body);
+
+    if(!isValid) {
+        return res.status(400).json(err);
+    }
+
+    User.findOne({email: req.body.email})
+        .then( user => {
+            if (user) {
+                err.email = 'There is an account associated with that Email'
+            } else {
+                const newUser = new User({
+                    name: req.body.name,
+                    email: req.body.email,
+                    password: req.body.password,
+                })
+            }
+        })
+})
+
+router.post('/login', (req, res) => {
+    const { err, isValid } = validateLoginInput(req.body);
+
+    if(!isValid) {
+        return res.status(400).json(err);
+    }
+
+    User.findOne({email})
+        .then(user => {
+            if(!user){
+                err.email = 'User not found';
+                return res.status(404).json(err)
+            }
+
+            bcrypt.compare(password, user.password)
+                .then(isMatch => {
+                    if (isMatch) {
+                        res.json({msg: 'Sucessfully Logged In!'})
+                    } else {
+                        err.password = 'Incorrect Password'
+                        return res.status(400).json(err)
+                    }
+                })
+        })
+})
 
 router.get("/test", (req, res) => res.json({
     msg: "This is the users route"
